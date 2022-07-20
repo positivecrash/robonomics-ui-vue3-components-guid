1ff0e359d6fc51c10e4a21c3ef36bdb7142b5ed1
A. Bakai @positivecrash <nastin_box@list.ru>
2022-07-20 19:43:03 +0300

    Updates for accounts

---
+++ src/views/Changelog.vue
@@ -0,0 +1,48 @@
+<template>
+    <h1>Changelog</h1>
+
+    <guide-section
+      title = "0.0.1-23"
+    >
+    <ol>
+        <li>
+            <h4>Renamed property for <router-link to="account-polkadot">robo-account-polkadot</router-link></h4>
+            <code>allowShiftExtensions</code> -> <code>extensionAllowShift</code>
+        </li>
+        <li>
+            <h4>New functionality for <router-link to="account-polkadot">robo-account-polkadot</router-link></h4>
+            <ul>
+                <li>
+                    Forced chain for address
+                </li>
+                <li>
+                    Not linked address
+                </li>
+                <li>
+                    Allow edit not linked address
+                </li>
+                <li>
+                    Callbacks: beforeInjected, afterInjected, onAddressChange
+                </li>
+                <li>
+                    Added <code>$store.state.robonomicsUIvue.polkadot.accounts</code> with all injected accounts (from selected extension)
+                </li>
+            </ul>
+        </li>
+    </ol>
+    </guide-section>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent ({
+  name: 'Changelog',
+})
+</script>
+
+<style scoped>
+h4 {
+    text-align: left;
+}
+</style>
