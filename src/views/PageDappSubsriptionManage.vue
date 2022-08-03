a84874823839083fa8822b24ba27416008cd6eab
A. Bakai @positivecrash <nastin_box@list.ru>
2022-08-03 19:28:05 +0300

    related services in subscribtion

--- src/views/PageDappSubsriptionManage.vue
+++ src/views/PageDappSubsriptionManage.vue
@@ -32,11 +32,6 @@
                                         </robo-text>
                                     </robo-list-item>
 
-                                    <robo-account-polkadot />
-                                    <robo-account-polkadot selectable chain="0"/>
-                                    <robo-account-polkadot chain="0" short />
-                                    <robo-account-polkadot addressLocal="15CzEmZRy5Az2YNADEEuweXVe8XxaHbxHJjVZcd9rGuHuPCg" chain="0" short/>
-                                    <robo-account-polkadot addressLocal="15CzEmZRy5Az2YNADEEuweXVe8XxaHbxHJjVZcd9rGuHuPCg" addressLocalAllowEdit chain="32" />
                                 </robo-list>
                             </robo-card-section>
                         </robo-card>
@@ -127,6 +122,63 @@
                                 </robo-section>
 
 
+                            </robo-card-section>
+                        </robo-card>
+                    </robo-section>
+
+                    <robo-section>
+                        <robo-card>
+                            <robo-card-label>
+                                <robo-card-label-section>Related services</robo-card-label-section>
+                            </robo-card-label>
+
+                            <robo-card-section>
+                                <robo-grid columnsRepeat="3" offset="x0">
+                                    <robo-template-services-item 
+                                        href="#"
+                                        label="demo"
+                                        picture="./images/services-ha.jpg"
+                                        :providers="[
+                                        {
+                                            name: 'Robonomics Core Team',
+                                            link: 'https://robonomics.network'
+                                        }
+                                        ]"
+                                        title="Home Assistant account"
+                                    >
+                                        Get Home Assistant account for Robonomics Parachain usage
+                                    </robo-template-services-item>
+
+                                    <robo-template-services-item 
+                                        href="#"
+                                        label="demo"
+                                        picture="./images/services-ha.jpg"
+                                        :providers="[
+                                        {
+                                            name: 'Robonomics Core Team',
+                                            link: 'https://robonomics.network'
+                                        }
+                                        ]"
+                                        title="SmartHome Telemetry"
+                                    >
+                                        Recieve and decrypt the telemetry from your smart home IoT devices 
+                                    </robo-template-services-item>
+                                    
+                                    <robo-template-services-item 
+                                        :router="{ name: 'Home' }"
+                                        label="demo"
+                                        picture="./images/services-lights-up.jpg"
+                                        :providers="[{
+                                            name: 'Robonomics Core Team',
+                                            link: 'https://robonomics.network'
+                                        }]"
+                                        title="Lights up!"
+                                    >
+                                        Set color for smart lamp via Robonomics Subscription - Decentrilized IoT cloud alternative.
+                                    </robo-template-services-item>
+                                    
+                                    
+                                </robo-grid>
                             </robo-card-section>
                         </robo-card>
                     </robo-section>
