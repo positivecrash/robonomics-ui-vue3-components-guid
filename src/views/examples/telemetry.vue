2818f725e800d5dcfeaaaae7acd2f2594c4e05ad
A. Bakai @positivecrash <nastin_box@list.ru>
2023-06-13 13:17:42 +0300

    statuses for launches

--- src/views/examples/telemetry.vue
+++ src/views/examples/telemetry.vue
@@ -18,13 +18,13 @@
 
 <script setup>
 import { ref, onMounted, watch } from 'vue'
-// import datalog from '../../data/shtab2/telemetry.json'
-// import datalog2 from '../../data/shtab2/telemetry2.json'
-// import config from '../../data/shtab2/config.json'
+import datalog from '../../data/shtab2/telemetry.json'
+import datalog2 from '../../data/shtab2/telemetry2.json'
+import config from '../../data/shtab2/config.json'
 
-import datalog from '../../data/shtab/telemetry.json'
-import datalog2 from '../../data/shtab/telemetry.json'
-import config from '../../data/shtab/config.json'
+// import datalog from '../../data/shtab/telemetry.json'
+// import datalog2 from '../../data/shtab/telemetry.json'
+// import config from '../../data/shtab/config.json'
 
 import dappHeader from '../../components/example/Header.vue'
 
@@ -38,9 +38,9 @@ import dappHeader from '../../components/example/Header.vue'
 // функция для эмуляции задержки загрузки данных из чейна
 let getDatalog = (type) => {
   return new Promise(res => {
-    let time = 2000;
+    let time = 500;
     if (type === "config") {
-      time = 3000;
+      time = 1000;
     }
     setTimeout(() => {
       if (type === "config") {
@@ -57,7 +57,6 @@ let getDatalog = (type) => {
 const configData = ref(null)
 const datalogData = ref(null)
 const updateTime = ref(null)
-// const launchStatus = ref(null)
 /* - test async */
 
 /* + get launch command */
@@ -65,14 +64,44 @@ import { useStore } from 'vuex'
 const store = useStore()
 
 onMounted( async () => {
+
     watch(() => store.state.robonomicsUIvue.rws.launch, value => {
-      console.log('LAUNCH', value)
-      updateTime.value = Date.now()
+
+      // пример обновления статус бара (сообщение будет показываться все время пока не перебьется новым, так как timeout: null)
       store.dispatch('app/setStatus', {value: 'Launch initiated', timeout: null})
-      // обновление статуса для лаунч
-      // setTimeout(async () => {
-      //   launchStatus.value = ['1684238715398', 'error', 'message test']
-      // }, 1000)
+
+      // обновление статуса для лаунч, тест
+      // возможные статусы для транзации: 'pending', 'approved', 'declined', 'success', 'error'
+      console.log('LAUNCH', value)
+      const o = JSON.parse(value)
+      
+      if(o['tx'].tx_status === 'pending'){
+        setTimeout(() => {
+          o['tx'].tx_status = 'approved'
+          store.commit('rws/setLaunch', JSON.stringify(o))
+        }, 1000)
+
+        setTimeout(() => {
+          o['tx'].tx_status = 'declined'
+          store.commit('rws/setLaunch', JSON.stringify(o))
+        }, 3000)
+
+        setTimeout(() => {
+          o['tx'].tx_status = 'success'
+          store.commit('rws/setLaunch', JSON.stringify(o))
+        }, 6000)
+
+        setTimeout(() => {
+          o['tx'].tx_status = 'error'
+          store.commit('rws/setLaunch', JSON.stringify(o))
+        }, 9000)
+
+        setTimeout(() => {
+          o['tx'].tx_status = 'none'
+          store.commit('rws/setLaunch', JSON.stringify(o))
+        }, 12000)
+
+      }
     })
 
     /* + test async */
@@ -88,7 +117,7 @@ onMounted( async () => {
       updateTime.value = Date.now()
       store.dispatch('app/setStatus', {value: 'Config & telemetry updated', timeout: 3000})
 
-    }, 10000)
+    }, 4000)
     
     /* - test async */
 })
